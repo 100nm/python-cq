47f46f4df58ece8c104b2254eeb9d04bc3a3b382
remimd <remimd@github.com>
2025-11-11 19:40:58 +0100

    refactoring: Message bus types

--- cq/_core/message.py
+++ cq/_core/message.py
@@ -2,6 +2,7 @@ from typing import Any, Final
 
 import injection
 
+from cq._core.dispatcher.base import Dispatcher
 from cq._core.dispatcher.bus import Bus, SimpleBus, TaskBus
 from cq._core.handler import (
     HandlerDecorator,
@@ -15,9 +16,9 @@ Command = object
 Event = object
 Query = object
 
-type CommandBus[T] = Bus[Command, T]
-type EventBus = Bus[Event, None]
-type QueryBus[T] = Bus[Query, T]
+type CommandBus[T] = Dispatcher[Command, T]
+type EventBus = Dispatcher[Event, None]
+type QueryBus[T] = Dispatcher[Query, T]
 
 AnyCommandBus = CommandBus[Any]
 
@@ -33,8 +34,7 @@ query_handler: Final[HandlerDecorator[Query, Any]] = HandlerDecorator(
 )
 
 
-@injection.injectable(inject=False, mode="fallback")
-def new_command_bus(*, threadsafe: bool | None = None) -> CommandBus:  # type: ignore[type-arg]
+def new_command_bus(*, threadsafe: bool | None = None) -> Bus[Command, Any]:
     bus = SimpleBus(command_handler.manager)
     transaction_scope_middleware = InjectionScopeMiddleware(
         CQScope.TRANSACTION,
@@ -45,11 +45,27 @@ def new_command_bus(*, threadsafe: bool | None = None) -> CommandBus:  # type: i
     return bus
 
 
-@injection.injectable(inject=False, mode="fallback")
-def new_event_bus() -> EventBus:
+def new_event_bus() -> Bus[Event, None]:
     return TaskBus(event_handler.manager)
 
 
-@injection.injectable(inject=False, mode="fallback")
-def new_query_bus() -> QueryBus:  # type: ignore[type-arg]
+def new_query_bus() -> Bus[Query, Any]:
     return SimpleBus(query_handler.manager)
+
+
+@injection.injectable(inject=False, mode="fallback")
+def _() -> CommandBus:  # type: ignore[type-arg]
+    return new_command_bus()
+
+
+@injection.injectable(inject=False, mode="fallback")
+def _() -> EventBus:
+    return new_event_bus()
+
+
+@injection.injectable(inject=False, mode="fallback")
+def _() -> QueryBus:  # type: ignore[type-arg]
+    return new_query_bus()
+
+
+del _
