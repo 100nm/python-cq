a485e6e478cf0bc23442548d23972b186a1aec4f
remimd <remimd@github.com>
2026-05-15 22:27:34 +0200

    feat: Add middlewares and concurrent draining to Pump

--- tests/core/test_pump.py
+++ tests/core/test_pump.py
@@ -1,6 +1,9 @@
-import anyio
+from typing import Any
 
-from cq import MemoryQueue, Pump
+import anyio
+import pytest
+
+from cq import MemoryQueue, MiddlewareResult, Pump
 
 
 class TestPump:
@@ -9,7 +12,7 @@ class TestPump:
 
         async def dispatcher(message: str) -> None:
             await anyio.sleep(60)
-            dispatched.set()
+            dispatched.set()  # pragma: no cover
 
         queue = MemoryQueue[str]()
 
@@ -17,3 +20,34 @@ class TestPump:
             await queue.send("message")
 
         assert not dispatched.is_set()
+
+    async def test_draining_with_middleware(self) -> None:
+        entered = anyio.Event()
+        exited = anyio.Event()
+
+        async def middleware(message: str) -> MiddlewareResult[Any]:
+            entered.set()
+            try:
+                yield
+            finally:
+                exited.set()
+
+        async def dispatcher(message: str) -> None: ...
+
+        queue = MemoryQueue[str]()
+
+        async with Pump(queue, dispatcher).add_middlewares(middleware).draining():
+            await queue.send("message")
+
+        assert entered.is_set() and exited.is_set()
+
+    async def test_draining_when_concurrency_is_less_than_1_raise_value_error(
+        self,
+    ) -> None:
+        async def dispatcher(message: str) -> None: ...
+
+        queue = MemoryQueue[str]()
+
+        with pytest.raises(ValueError):
+            async with Pump(queue, dispatcher).draining(concurrency=0):
+                raise NotImplementedError
